2d81e9e81e4cffc7c2d084c72b65b83e5073ff00
Benjmain Atsu <Bredrenbonish@gmail.com>
2024-04-26 01:38:08 +0000

    SQLAlchemy model named User for a database table named users

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,10 +9,11 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
+
 class User(Base):
     """SQLALchemy model for user"""
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
